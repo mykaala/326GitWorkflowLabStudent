c98ea41af8a1a5812555bffd1d72d7cfe812d3de
Ayushman Satpathy <ayushmansatpathy1234@gmail.com>
2024-10-11 13:12:26 -0400

    added cosine function

--- calculator.js
+++ calculator.js
@@ -32,8 +32,9 @@ function sine(value) {
 
 // TODO: Implement cosine function
 function cosine(value) {
-    // return the cosine of the value
+    return Math.cos(value);
 }
+
 
 // TODO: Implement tangent function
 function tangent(value) {
